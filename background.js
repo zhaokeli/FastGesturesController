81a7b424cdcc912338a696f787d7fa15e3052cf7
zkx <zkx@yg.com>
2022-09-16 23:25:52 +0800

    后台连接

---
+++ background.js
@@ -0,0 +1,50 @@
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+	console.log(message);
+	sendResponse('成功收到了');
+	connectHost();
+});
+
+// function updateResult(obj, state) {
+// 	document.getElementById(obj).innerHTML = state;
+// }
+var port = null;
+function connectHost() {
+	if (port) {
+		return
+	}
+	// var hostName = "com.fastgestures.agent";
+	// var port = chrome.runtime.connectNative(hostName);
+	//updateResult("result1", "已连接");
+	port = chrome.runtime.connectNative('com.fastgestures.agent');
+	port.onMessage.addListener(function (response) {
+		console.log("rev ", response);
+		setTimeout(() => {
+			port.postMessage({ text: (new Date()).getTime() });
+		}, 3000);
+
+	});
+	port.onDisconnect.addListener(function (response) {
+		console.log("disconnect", response);
+		// port.postMessage({ text: "我的应用程序，您好！" });
+		port = null;
+	});
+	// port.onConnect.addListener(function () {
+	// 	console.log("已连接");
+
+	// });
+	port.postMessage({ text: "我的应用程序，您好！" });
+}
+
+// function listener() {
+// 	updateResult("result2", "listen");
+// }
+// document.addEventListener('DOMContentLoaded', function () {
+// 	document.querySelector('#button1').addEventListener('click', invoke);
+// 	document.querySelector('#send').addEventListener('click', function () {
+// 		if (!port) {
+// 			return
+// 		}
+// 		port.postMessage({ text: "我的应用程序，您好！" });
+// 	});
+// });
+
--- popup.js
+++ popup.js
@@ -7,27 +7,32 @@ function invoke() {
 	if (port) {
 		return
 	}
+	// 给bg.js发消息
+	chrome.runtime.sendMessage({ greeting: 'someFrom' }, function (response) {
+		console.log(response);
+	});
+
 	// var hostName = "com.fastgestures.agent";
 	// var port = chrome.runtime.connectNative(hostName);
 	updateResult("result1", "已连接");
-	port = chrome.runtime.connectNative('com.fastgestures.agent');
-	port.onMessage.addListener(function (response) {
-		console.log("rev ", response);
-		setTimeout(() => {
-			port.postMessage({ text: (new Date()).getTime() });
-		}, 3000);
-
-	});
-	port.onDisconnect.addListener(function (response) {
-		console.log("disconnect", response);
-		port.postMessage({ text: "我的应用程序，您好！" });
-		port = null;
-	});
-	// port.onConnect.addListener(function () {
-	// 	console.log("已连接");
+	// port = chrome.runtime.connectNative('com.fastgestures.agent');
+	// port.onMessage.addListener(function (response) {
+	// 	console.log("rev ", response);
+	// 	setTimeout(() => {
+	// 		port.postMessage({ text: (new Date()).getTime() });
+	// 	}, 3000);
 
 	// });
-	port.postMessage({ text: "我的应用程序，您好！" });
+	// port.onDisconnect.addListener(function (response) {
+	// 	console.log("disconnect", response);
+	// 	port.postMessage({ text: "我的应用程序，您好！" });
+	// 	port = null;
+	// });
+	// // port.onConnect.addListener(function () {
+	// // 	console.log("已连接");
+
+	// // });
+	// port.postMessage({ text: "我的应用程序，您好！" });
 }
 
 function listener() {
