46e9c4a446d42a8190b2066ea7c8e87edf805b55
zkx <zkx@yg.com>
2022-09-20 20:38:51 +0800

    完善样式

--- popup.js
+++ popup.js
@@ -1,10 +1,19 @@
-
+function showElement(id, isShow) {
+	document.getElementById(id).style.display = isShow ? 'block' : 'none';
+}
 function checkStatus() {
 	// 给bg.js发消息，查询连接状态
 	chrome.runtime.sendMessage({ action: 'status' }, function (response) {
 		var obj = document.getElementById('status');
 		obj.innerHTML = response ? "已连接" : '未连接';
 		obj.className = response ? "status-text conn" : "status-text disconn";
+		if (response) {
+			showElement('conn', false);
+			showElement('disconn', true);
+		} else {
+			showElement('conn', true);
+			showElement('disconn', false);
+		}
 	});
 }
 document.addEventListener('DOMContentLoaded', function () {
@@ -31,12 +40,12 @@ document.addEventListener('DOMContentLoaded', function () {
 			checkStatus();
 		});
 	});
-	document.querySelector('#send').addEventListener('click', function () {
-		// 给bg.js发消息
-		chrome.runtime.sendMessage({ action: 'message', content: '我的应用程序，您好！' }, function (response) {
-			console.log(response);
-		});
+	// document.querySelector('#send').addEventListener('click', function () {
+	// 	// 给bg.js发消息
+	// 	chrome.runtime.sendMessage({ action: 'message', content: '我的应用程序，您好！' }, function (response) {
+	// 		console.log(response);
+	// 	});
 
-	});
+	// });
 });
 
